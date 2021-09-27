d043867690ea203d991b894a94cd7176ac9398e7
Mathieu Dugre <mathieu.dugre@mdugre.info>
2021-09-27 23:26:15 +0000

    wiring main

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,28 @@ mod domaines_maitredescles;
 mod maitredescles_partition;
 mod maitredescles_ca;
 
+use log::{info};
+use millegrilles_common_rust::tokio as tokio;
+use crate::domaines_maitredescles::build;
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    info!("Demarrer le contexte");
+    executer()
+}
+
+#[tokio::main(flavor = "current_thread")]
+// #[tokio::main(flavor = "multi_thread", worker_threads = 5)]
+async fn executer() {
+    build().await
+}
+
+#[cfg(test)]
+pub mod test_setup {
+    use log::{debug};
+
+    pub fn setup(nom: &str) {
+        let _ = env_logger::builder().is_test(true).try_init();
+        debug!("Running {}", nom);
+    }
 }
