444539be927f82943270e96f4d00105e57d0e993
Mathieu Dugre <mathieu.dugre@mdugre.info>
2022-06-16 13:42:03 -0400

    Re-emission cle batch rechiffrage si partition est recepteur

--- src/maitredescles_partition.rs
+++ src/maitredescles_partition.rs
@@ -830,13 +830,31 @@ async fn commande_sauvegarder_cle<M>(middleware: &M, m: MessageValideAction, ges
 
 async fn commande_rechiffrer_batch<M>(middleware: &M, m: MessageValideAction, gestionnaire: &GestionnaireMaitreDesClesPartition)
     -> Result<Option<MessageMilleGrille>, Box<dyn Error>>
-    where M: GenerateurMessages + MongoDao,
+    where M: GenerateurMessages + MongoDao + Chiffreur<CipherMgs3, Mgs3CipherKeys>
 {
     debug!("commande_rechiffrer_batch Consommer commande : {:?}", & m.message);
     let commande: CommandeRechiffrerBatch = m.message.get_msg().map_contenu(None)?;
     debug!("commande_rechiffrer_batch Commande parsed : {:?}", commande);
 
     let fingerprint = gestionnaire.fingerprint.as_str();
+    let enveloppe_privee = middleware.get_enveloppe_privee();
+    let fingerprint_ca = enveloppe_privee.enveloppe_ca.fingerprint.clone();
+
+    // Determiner si on doit rechiffrer pour d'autres maitre des cles
+    let cles_chiffrage = {
+        let mut cles_chiffrage = Vec::new();
+        for fingerprint_cert_cle in middleware.get_publickeys_chiffrage() {
+            let fingerprint_cle = fingerprint_cert_cle.fingerprint;
+            if fingerprint_cle != fingerprint && fingerprint_cle != fingerprint_ca {
+                cles_chiffrage.push(fingerprint_cert_cle.public_key);
+            }
+        }
+        cles_chiffrage
+    };
+
+    let routage_commande = RoutageMessageAction::builder(DOMAINE_NOM, COMMANDE_SAUVEGARDER_CLE)
+        .exchanges(vec![Securite::L4Secure])
+        .build();
 
     let collection = middleware.get_collection(gestionnaire.get_collection_cles().as_str())?;
 
@@ -853,15 +871,11 @@ async fn commande_rechiffrer_batch<M>(middleware: &M, m: MessageValideAction, ge
         let opts = UpdateOptions::builder().upsert(true).build();
         let resultat = collection.update_one(filtre, ops, opts).await?;
 
-        // if let Some(uid) = resultat.upserted_id {
-        //     debug!("commande_rechiffrer_batch Nouvelle cle insere _id: {}, generer transaction", uid);
-        //     let routage = RoutageMessageAction::builder(DOMAINE_NOM, TRANSACTION_CLE)
-        //         .partition(fingerprint)
-        //         .exchanges(vec![Securite::L4Secure])
-        //         .build();
-        //     middleware.soumettre_transaction(routage, &cle, false).await?;
-        // }
-
+        // Rechiffrer pour tous les autres maitre des cles
+        if cles_chiffrage.len() > 0 {
+            let commande_rechiffree = rechiffrer_pour_maitredescles(middleware, &cle)?;
+            middleware.transmettre_commande(routage_commande.clone(), &commande_rechiffree, false).await?;
+        }
     }
 
     // Emettre un evenement pour confirmer le traitement.
